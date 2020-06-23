20f5d59482ca37fe898dee61daa1f5464a5349b0
Kenneth Jensen <kenneth_jensen_99@hotmail.com>
2020-06-23 09:03:47 +0200

    asAS

--- index.php
+++ index.php
@@ -9,11 +9,19 @@
 
 <h2>test</h2>
 <h1>This is a Heading</h1>
+<<<<<<< Updated upstream
 kej
 sadasdssssdsfsdf
 <!--kljaskjdkljsad -->asdasd
 <!--kljaskjdkljsad -->sssnmb,sdan,m 
 sadsad
+=======
+
+
+<!--kljaskjdkljsad -->
+<!--kljaskjdkljsad -->
+
+>>>>>>> Stashed changes
 <!--kljaskjdkljsad -->
 <p>This is a paragraph.</p>
 TEST2
